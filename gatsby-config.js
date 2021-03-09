127392a6ef34a338d49bad3046746137becf2626
Vladimir Zhelvis <zhelvisvladimir@gmail.com>
2021-03-09 04:13:53 +0300

    add generated files

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#0000FF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ public/component---cache-caches-gatsby-plugin-offline-app-shell-js-a8880ddc54293fc869fe.js
@@ -0,0 +1,2 @@
+(self.webpackChunkleader_website=self.webpackChunkleader_website||[]).push([[306],{1930:function(e,t,n){"use strict";var u=n(5318);t.__esModule=!0,t.default=void 0;var r=u(n(5354)),a=u(n(7294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component);t.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-a8880ddc54293fc869fe.js.map
